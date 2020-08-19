28adb2ab5c151ade688780a3284aacfc1c7dafb5
mkdoh3 <mkdoherty3@gmail.com>
2020-08-19 13:56:27 -0500

    WIP: Adding Patient resource, show and index actions

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class PatientsController < ApplicationController
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find_by_id(params[:id])
+  end
+end
