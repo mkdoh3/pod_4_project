7ce3f82cb91347fbe8a6c3ccb3bfd7a5d9d0bec7
dnodolf1212 <dnodolf@gmail.com>
2020-08-26 11:11:55 -0700

    first commit for pod 4 project

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PatientsController < ApplicationController
+  
   def index
     @patients = Patient.all
   end
@@ -8,4 +9,41 @@ class PatientsController < ApplicationController
   def show
     @patient = Patient.find_by_id(params[:id])
   end
+
+  def new 
+    @patient = Patient.new
+  end
+
+  def create 
+    @patient = Patient.create(patient_params)
+    redirect_to patient_path(@patient)
+  end
+
+  def edit 
+    set_patient
+  end 
+
+  def update 
+    set_patient
+    @patient.update(patient_params)
+    redirect_to patient_path(@patient)
+  end
+
+  def destroy 
+    set_patient
+    @patient.destroy 
+    redirect_to patients_path
+  end
+
+  
+  private 
+
+  def set_patient 
+    @patient = Patient.find_by_id(params[:id])
+  end
+
+  def patient_params 
+    params.require(:patient).permit(:name, :age, :bmi, :phone_number)
+  end
+
 end
